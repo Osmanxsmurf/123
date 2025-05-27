c7e06373dfb20cad25dc076814485c703799c356
Osmanxsmurf <92298062+Osmanxsmurf@users.noreply.github.com>
2025-05-27 22:07:40 +0300

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,22 @@
-declare const plugin: { handler: () => void }
+/**
+Regular expression for matching a [shebang](https://en.wikipedia.org/wiki/Shebang_(Unix)) line.
 
-export = plugin
+@example
+```
+import shebangRegex = require('shebang-regex');
+
+const string = '#!/usr/bin/env node\nconsole.log("unicorns");';
+
+shebangRegex.test(string);
+//=> true
+
+shebangRegex.exec(string)[0];
+//=> '#!/usr/bin/env node'
+
+shebangRegex.exec(string)[1];
+//=> '/usr/bin/env node'
+```
+*/
+declare const shebangRegex: RegExp;
+
+export = shebangRegex;
